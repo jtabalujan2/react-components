f790523d3a1aafc88bd92d56c19a66a43e00ebaf
Julian Tab <heyyytabalu@yahoo.com>
2019-04-20 01:40:01 -0700

    Added state for latitude and moved geolocation to constructor

--- src/index.js
+++ src/index.js
@@ -2,15 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-    render(){
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            latitude: null
+        }
 
         window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
+            position => {
+                this.setState({
+                    latitude: position.coords.latitude
+                })
+            },
             err => console.log(err)
         )
-        
-        
-        return <div>Location:</div>
+
+    }
+
+
+
+    render(){
+
+        return <div>Location: {this.state.latitude}</div>
     }
 }
 
